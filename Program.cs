1e8f440270333278889a1345515bfa6aa7b30cb7
Khaoula ait <akaoula34@gmail.com>
2026-09-23 18:24:18 +0100

    Creating Calculator class

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
 ﻿
 namespace Calculator_App
 {
+
+    class Calculator
+    {
+        public string Name;
+
+        public Calculator(string name)
+        {
+            Name = name;
+        }
+      
+    }
     internal class Program
     {
         static void Main(string[] args)
